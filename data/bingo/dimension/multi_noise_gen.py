aea846baf721eff33c27870de2c8cb8a083eda12
NeunEinser <daniel30797@gmail.com>
2021-03-05 15:48:35 +0100

    Biome Gen: Add special biomes
    Adds special biomes.
    
    This also changes humidity noise slightly, but might need to do more
    changes.

--- data/bingo/dimension/multi_noise_gen.py
+++ data/bingo/dimension/multi_noise_gen.py
@@ -8,37 +8,57 @@ def write_biome(name, temperature, altitude, humidity, weirdness, offset):
 	file.write('\n\t\t\t\t\t\t"humidity": {},'.format(humidity))
 	file.write('\n\t\t\t\t\t\t"weirdness": {},'.format(weirdness))
 	file.write('\n\t\t\t\t\t\t"offset": {}\n\t\t\t\t\t}}\n\t\t\t\t}}'.format(offset))
-def write_biome_list(temperature, climate_biomes, hill_biomes, altitude, hill_altitude, iterations):
-	iterations = int(iterations / len(climate_biomes))
+def write_biome_list(temperature, climate_biomes, hill_biomes, altitude, hill_altitude, special_biome, special_hill_biome, iterations):
+	biome_duplications = int(iterations / len(climate_biomes))
+	special_biome_iter = -1
+	biome_count = len(climate_biomes)
+	total_iterations = biome_duplications * biome_count
 
-	for i in range(iterations):
+	if special_biome is not None:
+		special_biome_iter = random.randint(4, total_iterations - 4)
+		if special_biome_iter > (total_iterations / 2):
+			special_biome_iter += 2
+		else:
+			special_biome_iter -= 2
+		biome_count += 1
+		total_iterations += 1
+
+	for i in range(biome_duplications):
 		cur_biomes = climate_biomes.copy()
 		cur_hill_biomes = hill_biomes.copy()
-		step = 4.0 / (iterations * len(climate_biomes))
+		step = 4.0 / (biome_duplications * len(climate_biomes))
 
 		for j in range (len(cur_biomes)):
+			iteration = i * len(climate_biomes) + j
 			biome_index = random.randint(0, len(cur_biomes) - 1)
-			humidity = 2.0 - ((i * len(climate_biomes) + j + 0.5) * step)
 
-			write_biome(cur_biomes[biome_index], temperature, altitude, humidity, 0, 0)
+			biome = cur_biomes[biome_index]
+			hill_biome = cur_hill_biomes[biome_index]
+			if special_biome_iter == iteration:
+				biome = special_biome
+				hill_biome = special_hill_biome
+			else:
+				cur_biomes.remove(biome)
+				cur_hill_biomes.remove(hill_biome)
+
+			humidity = 2.0 - ((iteration + 0.5) * step)
+
+			write_biome(biome, temperature, altitude, humidity, 0, 0)
 			file.write(',')
 
-			hill_biome = cur_hill_biomes[biome_index]
 			if isinstance(hill_biome, list):
 				hill_biome = hill_biome[random.randint(0, len(hill_biome) - 1)]
 			write_biome(hill_biome, temperature, hill_altitude, humidity, 0, 0)
 
-			cur_biomes.remove(cur_biomes[biome_index])
-			cur_hill_biomes.remove(cur_hill_biomes[biome_index])
-			if not (i == iterations - 1 and len(cur_biomes) == 0):
+			if iteration < biome_duplications * len(climate_biomes) - 1:
 				file.write(',')
-def write_biome_list_for_temperature (temperature, climate_biomes, hill_biomes, ocean_biomes, deep_ocean_biomes, iterations):
-	write_biome_list(temperature, climate_biomes, hill_biomes, 0.1, 2, iterations)
+def write_biome_list_for_temperature (temperature, climate_biomes, hill_biomes, ocean_biomes, deep_ocean_biomes, special_biome, special_hill_biome, iterations):
+	write_biome_list(temperature, climate_biomes, hill_biomes, 0.1, 2, special_biome, special_hill_biome, iterations)
 	file.write(",")
-	write_biome_list(temperature, ocean_biomes, deep_ocean_biomes, -0.25, -1.3, iterations)
+	write_biome_list(temperature, ocean_biomes, deep_ocean_biomes, -0.25, -1.3, None, None, iterations)
 
 file = open(os.path.dirname(__file__) + "/multi_noise.json", "w")
-file.write('{\n\t"type": "minecraft:overworld",\n\t"generator": {\n\t\t"type": "minecraft:noise",\n\t\t"seed": 91,\n\t\t"settings": "minecraft:overworld",\n\t\t"biome_source": {\n\t\t\t"type": "minecraft:multi_noise",\n\t\t\t"seed": 91,\n\t\t\t"temperature_noise": {\n\t\t\t\t"firstOctave": -8,\n\t\t\t\t"amplitudes": [ 0, 0, 7, -2 ]\n\t\t\t},\n\t\t\t"altitude_noise": {\n\t\t\t\t"firstOctave": -7,\n\t\t\t\t"amplitudes": [ 0, 5, -10, 0.1, -1, 1 ]\n\t\t\t},\n\t\t\t"humidity_noise": {\n\t\t\t\t"firstOctave": -5,\n\t\t\t\t"amplitudes": [ 1.5, 1, -1.5, 0, 1.5, 1.1, -1.5, 0.5 ]\n\t\t\t},\n\t\t\t"weirdness_noise": {\n\t\t\t\t"firstOctave": -6,\n\t\t\t\t"amplitudes": [ 3 ]\n\t\t\t},\n\t\t\t"biomes": [')
+file.write('{\n\t"type": "minecraft:overworld",\n\t"generator": {\n\t\t"type": "minecraft:noise",\n\t\t"seed": 91,\n\t\t"settings": "minecraft:overworld",\n\t\t"biome_source": {\n\t\t\t"type": "minecraft:multi_noise",\n\t\t\t"seed": 91,\n\t\t\t"temperature_noise": {\n\t\t\t\t"firstOctave": -8,\n\t\t\t\t"amplitudes": [ 0, 0, 7, -2 ]\n\t\t\t},\n\t\t\t"altitude_noise": {\n\t\t\t\t"firstOctave": -7,\n\t\t\t\t"amplitudes": [ 0, 5, -10, 0.1, -1, 1 ]\n\t\t\t},\n\t\t\t"humidity_noise": {\n\t\t\t\t"firstOctave": -7,\n\t\t\t\t"amplitudes": [ 1.95, 1, -1.5, 0, 1.5, 1.1, -1.5, 0.5 ]\n\t\t\t},\n\t\t\t"weirdness_noise": {\n\t\t\t\t"firstOctave": -6,\n\t\t\t\t"amplitudes": [ 3 ]\n\t\t\t},\n\t\t\t"biomes": [')
 
 warm_biomes = [
 	"minecraft:desert", "minecraft:desert", "minecraft:desert",
@@ -124,13 +144,13 @@ ice_deep_ocean_biomes = [
 ]
 
 random.seed(91)
-write_biome_list_for_temperature(0.6, warm_biomes, warm_hill_biomes, warm_ocean_biomes, warm_deep_ocean_biomes, 13)
+write_biome_list_for_temperature(0.6, warm_biomes, warm_hill_biomes, warm_ocean_biomes, warm_deep_ocean_biomes, "minecraft:badlands_plateau", "minecraft:badlands", 24)
 file.write(',')
-write_biome_list_for_temperature(0.25, medium_biomes, medium_hill_biomes, medium_ocean_biomes, medium_deep_ocean_biomes, 13)
+write_biome_list_for_temperature(0.25, medium_biomes, medium_hill_biomes, medium_ocean_biomes, medium_deep_ocean_biomes, "minecraft:jungle", "minecraft:jungle_hills", 24)
 file.write(',')
-write_biome_list_for_temperature(-0.25, cold_biomes, cold_hill_biomes, cold_ocean_biomes, cold_deep_ocean_biomes, 13)
+write_biome_list_for_temperature(-0.25, cold_biomes, cold_hill_biomes, cold_ocean_biomes, cold_deep_ocean_biomes, "minecraft:giant_tree_taiga", "minecraft:giant_tree_taiga_hills", 24)
 file.write(',')
-write_biome_list_for_temperature(-0.6, ice_biomes, ice_hill_biomes, ice_ocean_biomes, ice_deep_ocean_biomes, 13)
+write_biome_list_for_temperature(-0.6, ice_biomes, ice_hill_biomes, ice_ocean_biomes, ice_deep_ocean_biomes, "minecraft:ice_spikes", "minecraft:ice_spikes", 24)
 
 file.write('\n\t\t\t]\n\t\t}\n\t}\n}')
 file.close()
