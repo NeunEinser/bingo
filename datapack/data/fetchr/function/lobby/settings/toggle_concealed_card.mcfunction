NeunEinser/bingo

commit 2007c3482804547e22c6dbf04f7bd8648fde6e49

Refactor translation string for settings

datapack/data/fetchr/function/lobby/settings/toggle_concealed_card.mcfunction
@@ -26,17 +26,17 @@ execute \
 	if score $concealed_card fetchr.setting_values matches 0 \
 	run tellraw @s [\
 		"",\
-		{ "translate": "fetchr.lobby.settings.concealed_card.toggle.off", "color": "red" },\
+		{ "translate": "fetchr.settings.concealed_card.confirmation.toggle_off", "color": "red" },\
 		"\n",\
-		{ "translate": "fetchr.lobby.settings.concealed_card.toggle.info", "color": "gray", "italic": true }\
+		{ "translate": "fetchr.settings.concealed_card.toggle.info", "color": "gray", "italic": true }\
 	]
 execute \
 	if score $concealed_card fetchr.setting_values matches 1 \
 	run tellraw @s [\
 		"",\
-		{ "translate": "fetchr.lobby.settings.concealed_card.toggle.on", "color": "green" },\
+		{ "translate": "fetchr.settings.concealed_card.confirmation.toggle_on", "color": "green" },\
 		"\n",\
-		{ "translate": "fetchr.lobby.settings.concealed_card.toggle.info", "color": "gray", "italic": true }\
+		{ "translate": "fetchr.settings.info.takes_effect_after_generating_new", "color": "gray", "italic": true }\
 	]
 
 #NEUN_SCRIPT until 69
@@ -46,14 +46,14 @@ execute \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.concealed_card_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.concealed_card.sign.off", "color": "gray" }'
+		set value '{ "translate": "options.off", "color": "gray" }'
 #execute \
 	if score $concealed_card fetchr.setting_values matches 1 \
 	run data \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.concealed_card_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.concealed_card.sign.on", "color": "green" }'
+		set value '{ "translate": "options.on", "color": "green" }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 execute \
@@ -63,7 +63,7 @@ execute \
 		modify \
 			entity @s \
 			data.front_text.messages[3] \
-		set value { "translate": "fetchr.lobby.settings.concealed_card.sign.off", "color": "gray" }
+		set value { "translate": "options.off", "color": "gray" }
 execute \
 	if score $concealed_card fetchr.setting_values matches 1 \
 	as @e[type=minecraft:marker, tag=fetchr.concealed_card_sign, x=0] \
@@ -71,7 +71,7 @@ execute \
 		modify \
 			entity @s \
 			data.front_text.messages[3] \
-		set value { "translate": "fetchr.lobby.settings.concealed_card.sign.on", "color": "green" }
+		set value { "translate": "options.on", "color": "green" }
 #NEUN_SCRIPT end
 
 function fetchr:lobby/place_indestructible_blocks

datapack/data/fetchr/function/game/end.mcfunction
@@ -40,7 +40,6 @@ scoreboard players add $current_game_id fetchr.game_id 1
 scoreboard players set $game_state fetchr.state 0
 
 scoreboard players operation $concealed_card fetchr.state = $concealed_card fetchr.setting_values
-scoreboard players operation $lockout_mode fetchr.state = $lockout_mode fetchr.setting_values
 team leave @a[predicate=fetchr:is_in_lobby]
 
 tag @a remove fetchr.automatically_join_game

datapack/data/fetchr/function/game/start/start_spawn_sequence.mcfunction
@@ -27,6 +27,7 @@ tag @a remove fetchr.in_skybox
 effect give @a[predicate=fetchr:is_in_game] minecraft:invisibility infinite 1 true
 clear @a
 
+scoreboard players operation $lockout_mode fetchr.state = $lockout_mode fetchr.setting_values
 execute \
 	in fetchr:lobby \
 	run function fetchr:game/start/init_teams

datapack/data/fetchr/function/lobby/settings/set_points_goal.mcfunction
@@ -22,17 +22,17 @@ scoreboard players enable @s fetchr.points_race_minutes
 #NEUN_SCRIPT until 77
 #tellraw @s [\
 	"\n\n\n\n\n=== ",\
-	{ "translate": "fetchr.lobby.settings.points_goal.menu.heading", "bold": true, "color": "green" },\
+	{ "translate": "fetchr.settings.points_goal.name", "bold": true, "color": "green" },\
 	" ===\n\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.off",\
+		"translate": "fetchr.settings.points_goal.menu.option.off",\
 		"color": "#00c3ff",\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set -1" },\
 		"click_event": { "action": "run_command", "command": "trigger fetchr.points_race_minutes set -1" }\
 	}\
 	, "]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["10"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 10" },\
@@ -40,7 +40,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["15"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 15" },\
@@ -48,7 +48,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["20"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 20" },\
@@ -56,7 +56,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["25"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 25" },\
@@ -64,7 +64,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["30"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 30" },\
@@ -72,7 +72,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["40"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 40" },\
@@ -80,7 +80,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["45"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 45" },\
@@ -88,7 +88,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.minutes",\
+		"translate": "fetchr.settings.points_goal.menu.option.minutes",\
 		"color": "#00c3ff",\
 		"with": ["60"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.points_race_minutes set 60" },\
@@ -96,7 +96,7 @@ scoreboard players enable @s fetchr.points_race_minutes
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.points_goal.menu.option.custom",\
+		"translate": "fetchr.settings.points_goal.menu.option.custom",\
 		"color": "#00c3ff",\
 		"clickEvent": { "action": "suggest_command", "value": "/trigger fetchr.points_race_minutes set "},\
 		"click_event": { "action": "suggest_command", "command": "/trigger fetchr.points_race_minutes set "}\

datapack/data/fetchr/function/lobby/settings/set_points_goal_internal.mcfunction
@@ -10,30 +10,30 @@
 scoreboard players operation $points_goal_announcement_minutes fetchr.setting_values = @s fetchr.points_race_minutes
 #NEUN_SCRIPT until 69
 #data modify block 7 0 7 front_text.messages[0] set value '{\
-	"translate": "fetchr.lobby.settings.points_goal.sign.minutes",\
+	"translate": "fetchr.settings.points_goal.sign.minutes",\
 	"color": "green",\
 	"with": [\
-		{ "score": { "name": "$points_goal_announcement_minutes", "objective": "fetchr.settings" }}\
+		{ "score": { "name": "$points_goal_announcement_minutes", "objective": "fetchr.setting_values" }}\
 	]\
 }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 data modify block 7 0 7 front_text.messages[0] set value {\
-	translate: "fetchr.lobby.settings.points_goal.sign.minutes",\
+	translate: "fetchr.settings.points_goal.value.minutes",\
 	color: "green",\
 	with: [\
-		{ score: { name: "$points_goal_announcement_minutes", objective: "fetchr.settings" }}\
+		{ score: { name: "$points_goal_announcement_minutes", objective: "fetchr.setting_values" }}\
 	]\
 }
 #NEUN_SCRIPT end
 
 execute \
 	if score @s fetchr.points_race_minutes matches ..0 \
-	run tellraw @s { "translate": "fetchr.lobby.settings.points_goal.menu.set.off", "color": "green" }
+	run tellraw @s { "translate": "fetchr.settings.points_goal.confirmation.toggle_off", "color": "green" }
 execute \
 	if score @s fetchr.points_race_minutes matches 1.. \
 	run tellraw @s {\
-		"translate": "fetchr.lobby.settings.points_goal.menu.set.minutes",\
+		"translate": "fetchr.settings.points_goal.confirmation.set_value",\
 		"color": "green",\
 		"with": [{ "score": { "name": "@s", "objective": "fetchr.points_race_minutes" }}]\
 	}
@@ -44,7 +44,7 @@ execute \
 	as @e[type=minecraft:marker, tag=fetchr.points_goal_sign, distance=0.., limit=2] \
 	run data \
 		modify entity @s data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.points_goal.sign.off", "color": "gray" }'
+		set value '{ "translate": "options.off", "color": "gray" }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 execute \
@@ -52,7 +52,7 @@ execute \
 	as @e[type=minecraft:marker, tag=fetchr.points_goal_sign, distance=0.., limit=2] \
 	run data \
 		modify entity @s data.front_text.messages[3] \
-		set value { translate: "fetchr.lobby.settings.points_goal.sign.off", color: "gray" }
+		set value { translate: "options.off", color: "gray" }
 #NEUN_SCRIPT end
 execute \
 	if score @s fetchr.points_race_minutes matches 1.. \

datapack/data/fetchr/function/lobby/settings/set_pre_gen_radius.mcfunction
@@ -21,20 +21,20 @@ scoreboard players enable @s fetchr.pre_gen_radius
 #tellraw @s [\
 	"\n\n\n\n\n=== ",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.heading",\
+		"translate": "fetchr.settings.pre_gen.menu.heading",\
 		"bold": true,\
 		"color": "green"\
 	},\
 	" ===\n\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.minimal",\
+		"translate": "fetchr.settings.pre_gen.menu.option.minimal",\
 		"color": "#00c3ff",\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 144" },\
 		"click_event": { "action": "run_command", "command": "trigger fetchr.pre_gen_radius set 144" }\
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["250"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 250" },\
@@ -42,7 +42,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["500"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 500" },\
@@ -50,7 +50,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["750"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 750" },\
@@ -58,7 +58,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["1000"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 1000" },\
@@ -66,7 +66,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["1500"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 1500" },\
@@ -74,7 +74,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.radius",\
+		"translate": "fetchr.settings.pre_gen.menu.option.radius",\
 		"color": "#00c3ff",\
 		"with": ["2000"],\
 		"clickEvent": { "action": "run_command", "value": "/trigger fetchr.pre_gen_radius set 2000" },\
@@ -82,7 +82,7 @@ scoreboard players enable @s fetchr.pre_gen_radius
 	},\
 	"]\n[",\
 	{\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.option.custom",\
+		"translate": "fetchr.settings.pre_gen.menu.option.custom",\
 		"color": "#00c3ff",\
 		"clickEvent": { "action": "suggest_command", "value": "/trigger fetchr.pre_gen_radius set " },\
 		"click_event": { "action": "suggest_command", "command": "/trigger fetchr.pre_gen_radius set "}\

datapack/data/fetchr/function/lobby/settings/set_pre_gen_radius_internal.mcfunction
@@ -11,7 +11,7 @@
 execute \
 	if score @s fetchr.pre_gen_radius matches ..143 \
 	run tellraw @s {\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.error.too_low",\
+		"translate": "fetchr.settings.pre_gen.error.too_low",\
 		"color": "red",\
 		"with": [\
 			{ "score": { "name": "@s", "objective": "fetchr.pre_gen_radius" }},\
@@ -25,16 +25,16 @@ execute \
 scoreboard players operation $pregeneration_chunks fetchr.setting_values = @s fetchr.pre_gen_radius
 #NEUN_SCRIPT until 69
 #data modify block 7 0 7 front_text.messages[0] set value '{\
-	"translate": "fetchr.lobby.settings.pre_gen.sign.radius",\
+	"translate": "fetchr.settings.pre_gen.value.radius",\
 	"color": "green",\
-	"with": [{ "score": { "name": "$pregeneration_chunks", "objective": "fetchr.settings" }}]\
+	"with": [{ "score": { "name": "$pregeneration_chunks", "objective": "fetchr.setting_values" }}]\
 }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 data modify block 7 0 7 front_text.messages[0] set value {\
-	translate: "fetchr.lobby.settings.pre_gen.sign.radius",\
+	translate: "fetchr.settings.pre_gen.value.radius",\
 	color: "green",\
-	with: [{ score: { name: "$pregeneration_chunks", objective: "fetchr.settings" }}]\
+	with: [{ score: { name: "$pregeneration_chunks", objective: "fetchr.setting_values" }}]\
 }
 #NEUN_SCRIPT end
 scoreboard players operation $pregeneration_chunks fetchr.setting_values /= 16 fetchr.const
@@ -44,11 +44,11 @@ scoreboard players operation $pregeneration_chunks fetchr.setting_values *= $pre
 
 execute \
 	if score @s fetchr.pre_gen_radius matches 144 \
-	run tellraw @s { "translate": "fetchr.lobby.settings.pre_gen.menu.set.minimal", "color": "green" }
+	run tellraw @s { "translate": "fetchr.settings.pre_gen.confirmation.set_minimal", "color": "green" }
 execute \
 	if score @s fetchr.pre_gen_radius matches 145.. \
 	run tellraw @s {\
-		"translate": "fetchr.lobby.settings.pre_gen.menu.set.radius",\
+		"translate": "fetchr.settings.pre_gen.confirmation.set_radius",\
 		"color": "green",\
 		"with": [{ "score": { "name": "@s", "objective": "fetchr.pre_gen_radius" }}]\
 	}
@@ -58,14 +58,14 @@ execute \
 	if score @s fetchr.pre_gen_radius matches 144 \
 	run data \
 		modify entity @e[type=minecraft:marker, tag=fetchr.pre_gen_sign, distance=0.., limit=1] data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.pre_gen.sign.minimal", "color": "gray" }'
+		set value '{ "translate": "fetchr.settings.pre_gen.value.minimal", "color": "gray" }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 execute \
 	if score @s fetchr.pre_gen_radius matches 144 \
 	run data \
 		modify entity @e[type=minecraft:marker, tag=fetchr.pre_gen_sign, distance=0.., limit=1] data.front_text.messages[3] \
-		set value { translate: "fetchr.lobby.settings.pre_gen.sign.minimal", color: "gray" }
+		set value { translate: "fetchr.settings.pre_gen.value.minimal", color: "gray" }
 #NEUN_SCRIPT end
 execute \
 	if score @s fetchr.pre_gen_radius matches 145.. \

datapack/data/fetchr/function/lobby/settings/toggle_allow_spectating.mcfunction
@@ -22,10 +22,10 @@ execute \
 
 execute \
 	if score $allow_spectating fetchr.setting_values matches 0 \
-	run tellraw @s { "translate": "fetchr.lobby.settings.allow_spectating.toggle.off", "color": "red" }
+	run tellraw @s { "translate": "fetchr.settings.allow_spectating.confirmation.toggle_off", "color": "red" }
 execute \
 	if score $allow_spectating fetchr.setting_values matches 1 \
-	run tellraw @s { "translate": "fetchr.lobby.settings.allow_spectating.toggle.on", "color": "green" }
+	run tellraw @s { "translate": "fetchr.settings.allow_spectating.confirmation.toggle_on", "color": "green" }
 
 #NEUN_SCRIPT until 69
 #execute \
@@ -34,14 +34,14 @@ execute \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.allow_spectating_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.allow_spectating.sign.off", "color": "gray" }'
+		set value '{ "translate": "options.off", "color": "gray" }'
 #execute \
 	if score $allow_spectating fetchr.setting_values matches 1 \
 	run data \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.allow_spectating_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value '{ "translate": "fetchr.lobby.settings.allow_spectating.sign.on", "color": "green" }'
+		set value '{ "translate": "options.on", "color": "green" }'
 #NEUN_SCRIPT end
 #NEUN_SCRIPT since 69
 execute \
@@ -50,14 +50,14 @@ execute \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.allow_spectating_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value { translate: "fetchr.lobby.settings.allow_spectating.sign.off", color: "gray" }
+		set value { translate: "options.off", color: "gray" }
 execute \
 	if score $allow_spectating fetchr.setting_values matches 1 \
 	run data \
 		modify \
 			entity @e[type=minecraft:marker, tag=fetchr.allow_spectating_sign, distance=..1, limit=1] \
 			data.front_text.messages[3] \
-		set value { translate: "fetchr.lobby.settings.allow_spectating.sign.on", color: "green" }
+		set value { translate: "options.on", color: "green" }
 #NEUN_SCRIPT end
 
 function fetchr:lobby/place_indestructible_blocks

commit e0685ec3c9e3ae7e1297e759aa3d46b14a7ff8bb

Fix #302
Caused by 2007c3482804547e22c6dbf04f7bd8648fde6e49

datapack/data/fetchr/function/lobby/settings/toggle_concealed_card.mcfunction
@@ -28,7 +28,7 @@ execute \
 		"",\
 		{ "translate": "fetchr.settings.concealed_card.confirmation.toggle_off", "color": "red" },\
 		"\n",\
-		{ "translate": "fetchr.settings.concealed_card.toggle.info", "color": "gray", "italic": true }\
+		{ "translate": "fetchr.settings.info.takes_effect_after_generating_new", "color": "gray", "italic": true }\
 	]
 execute \
 	if score $concealed_card fetchr.setting_values matches 1 \